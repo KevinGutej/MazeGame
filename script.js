a366d8370de85ce18bad7502362bd92cd4fa25cf
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-10-24 21:34:24 +0200

    Creating player moves

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ const mazeElement = document.getElementById("maze");
 
 const player = document.createElement("div");
         player.className = "player";
+        let playerRow = 1;
+        let playerCol = 1;
+
+        function movePlayer(e) {
+            const key = e.key;
+            const UpdatedRow = playerRow;
+            const UpdatednewCol = playerCol;
+
+
+        if (key === "ArrowUp") {
+            UpdatedRow--;
+        } else if (key === "ArrowDown") {
+            UpdatedRow++;
+        } else if (key === "ArrowLeft") {
+            UpdatednewCol--;
+        } else if (key === "ArrowRight") {
+            UpdatednewCol++;
+        }
+
+        if (maze[UpdatedRow][UpdatednewCol] !== "#") {
+            player.style.top = UpdatedRow * 30 + "px";
+            player.style.left = UpdatednewCol * 30 + "px";
+            playerRow = UpdatedRow;
+            playerCol = UpdatednewCol;
+        }
+    }
