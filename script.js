43036b7a0565a55361e24bdf1b52c45fe54b757b
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2023-10-26 21:33:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,10 +13,21 @@ const maze = [
 
 const mazeElement = document.getElementById("maze");
 
+function createMaze() {
+    for (let row = 0; row < maze.length; row++) {
+        for (let col = 0; col < maze[row].length; col++) {
+            const cell = document.createElement("div");
+        }
+    }
+}
+
 const player = document.createElement("div");
         player.className = "player";
         let playerRow = 1;
         let playerCol = 1;
+        mazeElement.appendChild(player);
+
+
 
         function movePlayer(e) {
             const key = e.key;
@@ -39,5 +50,9 @@ const player = document.createElement("div");
             player.style.left = UpdatednewCol * 30 + "px";
             playerRow = UpdatedRow;
             playerCol = UpdatednewCol;
+
+            if (maze[UpdatedRow][UpdatednewCol] === "E") {
+                alert("Well done, you have fount the exit");
+            }
         }
     }
